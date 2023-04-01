5524baf6adbdab10ead234abeefc06622112d08b
Matthew Lisitsky <43412072+mlisitsky@users.noreply.github.com>
2023-04-01 16:30:45 -0400

    added phrase set to resources and set up attributes for the test activity

--- app/src/main/java/ca/yorku/eecs/textinputcomparison/TestActivity.java
+++ app/src/main/java/ca/yorku/eecs/textinputcomparison/TestActivity.java
@@ -2,12 +2,193 @@ package ca.yorku.eecs.textinputcomparison;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/*
+Many thanks to I. Scott MacKenzie and R. William Soukoreff for creating the phrase set used here.
+For more information on the creation of this phrase set, you can read their paper on it here:
+http://www.yorku.ca/mack/chi03b.html
+ */
 
 public class TestActivity extends Activity {
+
+    private final static String PHRASES_LOCATION = "R.raw.phrases.txt";
+    private final static String CURRENT_QUESTION_NUMBER = "current_question_number";
+    private final static String PHRASES_LIST = "phrases_list";
+    private final static String PHRASE_LIST_NUMBER_OF_CHARACTERS = "phraseList_total_characters";
+    private final static String CURRENT_PHASE = "current_phase";
+    private final static String CURRENT_PHASE_START_TIME = "current_phase_start_time";
+    private final static String CURRENT_PHASE_NUMBER_OF_ERRORS = "current_errors";
+
+    private final static int HAPTIC_OFF = 0;
+    private final static String HAPTIC_OFF_START_TIME = "haptic_off_start_time";
+    private final static String HAPTIC_OFF_FINISH_TIME = "haptic_off_finish_time";
+    private final static String HAPTIC_OFF_NUMBER_OF_ERRORS = "haptic_off_errors";
+
+    private final static int HAPTIC_ON = 1;
+    private final static String HAPTIC_ON_START_TIME = "haptic_on_start_time";
+    private final static String HAPTIC_ON_FINISH_TIME = "haptic_on_finish_time";
+    private final static String HAPTIC_ON_NUMBER_OF_ERRORS= "haptic_on_errors";
+
+    private final static int VOICE_RECOGNITION = 2;
+    private final static String VOICE_RECOGNITION_START_TIME = "voice_recognition_start_time";
+    private final static String VOICE_RECOGNITION_FINISH_TIME = "voice_recognition_finish_time";
+    private final static String VOICE_RECOGNITION_NUMBER_OF_ERRORS = "voice_recognition_errors";
+    private final static int NUMBER_OF_QUESTIONS = 10;
+
+    int phraseListTotalCharacters;
+    int currentQuestionNumber, currentErrors, currentPhase;
+    long currentStartTime;
+
+    int hapticOffErrors, hapticOnErrors, voiceRecognitionErrors;
+    long hapticOffStartTime, hapticOffFinishTime, hapticOnStartTime, hapticOnFinishTime, voiceRecognitionStartTime, voiceRecognitionFinishTime;
+    float hapticOffWPM, hapticOffErrorRate, hapticOnWPM, hapticOnErrorRate, voiceRecognitionWPM, voiceRecognitionErrorRate;
+
+    TextView text_to_type;
+    EditText input_field;
+
+    ArrayList<String> testPhraseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+
+        text_to_type = findViewById(R.id.text_to_type);
+        input_field = findViewById(R.id.input_field);
+
+        phraseListTotalCharacters = 0;
+        testPhraseList.addAll(generatePhraseSet());
+        for (String s : testPhraseList) {
+            phraseListTotalCharacters += s.length();
+        }
+
+        text_to_type.setText(testPhraseList.get(currentQuestionNumber));
+
+        currentPhase = 0;      // Start with haptics off
+        currentErrors = 0;
+        currentStartTime = System.currentTimeMillis();
+
+        hapticOffStartTime = 0;
+        hapticOffFinishTime = 0;
+        hapticOffErrors = 0;
+
+        hapticOnStartTime = 0;
+        hapticOnFinishTime = 0;
+        hapticOnErrors = 0;
+
+        voiceRecognitionStartTime = 0;
+        voiceRecognitionFinishTime = 0;
+        voiceRecognitionErrors = 0;
+
+        hapticOffWPM = 0f;
+        hapticOffErrorRate = 0f;
+        hapticOnWPM = 0f;
+        hapticOnErrorRate = 0f;
+        voiceRecognitionWPM = 0f;
+        voiceRecognitionErrorRate = 0f;
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Phrase List Info
+        testPhraseList = savedInstanceState.getStringArrayList(PHRASES_LIST);
+        phraseListTotalCharacters = savedInstanceState.getInt(PHRASE_LIST_NUMBER_OF_CHARACTERS);
+
+        // Current Phase
+        currentPhase = savedInstanceState.getInt(CURRENT_PHASE);
+        currentQuestionNumber = savedInstanceState.getInt(CURRENT_QUESTION_NUMBER);
+        currentStartTime = savedInstanceState.getLong(CURRENT_PHASE_START_TIME);
+        currentErrors = savedInstanceState.getInt(CURRENT_PHASE_NUMBER_OF_ERRORS);
+
+        // Haptics Off Phase
+        hapticOffStartTime = savedInstanceState.getLong(HAPTIC_OFF_START_TIME);
+        hapticOffFinishTime = savedInstanceState.getLong(HAPTIC_OFF_FINISH_TIME);
+        hapticOffErrors = savedInstanceState.getInt(HAPTIC_OFF_NUMBER_OF_ERRORS);
+
+        // Haptics On Phase
+        hapticOnStartTime = savedInstanceState.getLong(HAPTIC_ON_START_TIME);
+        hapticOnFinishTime = savedInstanceState.getLong(HAPTIC_ON_FINISH_TIME);
+        hapticOnErrors = savedInstanceState.getInt(HAPTIC_OFF_NUMBER_OF_ERRORS);
+
+        // Voice Recognition Phase
+        voiceRecognitionStartTime = savedInstanceState.getLong(VOICE_RECOGNITION_START_TIME);
+        voiceRecognitionFinishTime = savedInstanceState.getLong(VOICE_RECOGNITION_FINISH_TIME);
+        voiceRecognitionErrors = savedInstanceState.getInt(VOICE_RECOGNITION_NUMBER_OF_ERRORS);
+
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        // Phrase List Info
+        savedInstanceState.putStringArrayList(PHRASES_LIST, testPhraseList);
+        savedInstanceState.putInt(PHRASE_LIST_NUMBER_OF_CHARACTERS, phraseListTotalCharacters);
+
+        // Current Phase
+        savedInstanceState.putInt(CURRENT_PHASE,currentPhase);
+        savedInstanceState.putInt(CURRENT_QUESTION_NUMBER,currentQuestionNumber);
+        savedInstanceState.putLong(CURRENT_PHASE_START_TIME, currentStartTime);
+        savedInstanceState.putInt(CURRENT_PHASE_NUMBER_OF_ERRORS, currentErrors);
+
+        // Haptics Off Phase
+        savedInstanceState.putLong(HAPTIC_OFF_START_TIME, hapticOffStartTime);
+        savedInstanceState.putLong(HAPTIC_OFF_FINISH_TIME, hapticOffFinishTime);
+        savedInstanceState.putInt(HAPTIC_OFF_NUMBER_OF_ERRORS, hapticOffErrors);
+
+        // Haptics On Phase
+        savedInstanceState.putLong(HAPTIC_ON_START_TIME, hapticOnStartTime);
+        savedInstanceState.putLong(HAPTIC_ON_FINISH_TIME, hapticOnFinishTime);
+        savedInstanceState.putInt(HAPTIC_ON_NUMBER_OF_ERRORS, hapticOnErrors);
+
+        // Voice Recognition Phase
+        savedInstanceState.putLong(VOICE_RECOGNITION_START_TIME, voiceRecognitionStartTime);
+        savedInstanceState.putLong(VOICE_RECOGNITION_FINISH_TIME, voiceRecognitionFinishTime);
+        savedInstanceState.putInt(VOICE_RECOGNITION_NUMBER_OF_ERRORS, voiceRecognitionErrors);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+
+    protected List<String> generatePhraseSet() {
+        ArrayList<String> fullPhraseList = new ArrayList<>();
+        ArrayList<String> curatedPhrases = new ArrayList<>();
+
+        currentQuestionNumber = 0;
+        try {
+            FileReader fReader = new FileReader(PHRASES_LOCATION);
+            BufferedReader bReader = new BufferedReader(fReader);
+            String line;
+
+            // Add each line of the initial phrase list text file to phrases as a String
+            while ((line = bReader.readLine()) != null) {
+                fullPhraseList.add(line);
+            }
+            fReader.close();
+
+            Random rand = new Random();
+
+            // Pull ten random items from the initial phrase list and add them to test phrase list
+            for (int i = 0; i < NUMBER_OF_QUESTIONS; i++) {
+                int listItemNumber = rand.nextInt(fullPhraseList.size() - 1);
+                curatedPhrases.add(fullPhraseList.get(listItemNumber));
+            }
+        }
+        catch (FileNotFoundException e) {
+
+        }
+        catch (IOException e) {
+
+        }
+        return curatedPhrases;
     }
 }
